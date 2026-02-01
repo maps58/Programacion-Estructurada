4d6efcae52bce267af9cf4bde18daa8ddf1a48ed
Miguel Angel Perez Solano <138253801+maps58@users.noreply.github.com>
2026-02-01 14:50:29 -0600

    Add files via upload

---
+++ 1.- Hola mundo.c
@@ -0,0 +1,19 @@
+
+
+/*Miguel Angel Pérez Solano
+Instituto Tecnológico de Oaxaca
+Departamento de Ingeniería Eléctrica y electronica*/
+
+//Programa "Hola Mundo".
+
+ #include <stdio.h>
+ 
+ int main(){
+ 	
+ printf("\tINSTITUTO TECNOLOGICO DE OAXACA\n");	
+ printf("\tCarrera: Ingeniería Electronica\n");
+ printf("Departamento de Ingeniería Eléctrica y Electrónica");
+ 	
+ 	return 0;
+ }
+ 
