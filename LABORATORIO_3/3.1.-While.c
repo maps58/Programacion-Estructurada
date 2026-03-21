1c87e30fa8f76f233811396e533d7736c3f36b64
Miguel Angel Perez Solano <138253801+maps58@users.noreply.github.com>
2026-03-21 08:09:19 -0600

    Update 3.1.-While.c

--- LABORATORIO_3/3.1.-While.c
+++ LABORATORIO_3/3.1.-While.c
@@ -1,17 +1,18 @@
 /*Instituto Tecnologico de Oaxaca
-Materia: Programacion estructurada*/
-
+Materia: Programacion estructurada
+Autor: Miguel Angel Pérez Solano
+Departamento de Ingniería eléctrica y Electronica*/
 
 #include <stdio.h>
  int main () {
-   /* local variable definition */
+   // Definicion de variables locales
    int min = 0;
    int max = 0;
 
    printf("ingresar el valor maximo y minino separados con guion bajo >>  \n"),
    scanf ("%d_%d", &min,&max);
 
-   /* while loop execution */
+   // Ejecución del bucle while
    while( min <= max ) {
       printf("value of a: %d\n", min);
       min++;  
