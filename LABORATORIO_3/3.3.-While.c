2eeda348b7933082897b731c7ef164aea375b0ff
Miguel Angel Perez Solano <138253801+maps58@users.noreply.github.com>
2026-03-21 08:15:13 -0600

    Revise comments and add author details
    Updated comments and author information in the code.

--- LABORATORIO_3/3.3.-While.c
+++ LABORATORIO_3/3.3.-While.c
@@ -1,5 +1,8 @@
-/*Instituto Tecnologico de Oaxaca
-Materia: Programacion estructurada*/
+/*Instituto Tecnológico de Oaxaca
+Materia: Programación estructurada
+Autor: Miguel Angel Pérez Solano
+Departamento de Ingeniería Eléctrica y Electronica*/
+
 #include <stdio.h>
 
 int main() {
