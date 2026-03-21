7afd594dd38cf249677fb90594ec82ea3a64385e
Miguel Angel Perez Solano <138253801+maps58@users.noreply.github.com>
2026-03-21 08:21:44 -0600

    Update 3.9.-Ciclo for.c

--- LABORATORIO_3/3.9.-Ciclo for.c
+++ LABORATORIO_3/3.9.-Ciclo for.c
@@ -1,10 +1,11 @@
 /*Instituto Tecnologico de Oaxaca
 Materia: Programacion estructurada*/
+
 #include <stdio.h>
  int main () {
 
    int a;
-   // ejecución del loop for
+   // ejecuciÃ³n del loop for
    for( a = 0; a <= 20; ++a){
       printf("valor de a: %d\n", a);
    } 
